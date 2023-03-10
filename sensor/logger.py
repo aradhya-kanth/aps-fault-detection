7fbdf319e8d4e2e1a791e885f18809a0cbc8aa6b
aradhya-kanth <aradhyakanth.mail.com>
2023-03-10 09:31:54 +0530

    uploaded in git

---
+++ sensor/logger.py
@@ -0,0 +1,18 @@
+import logging
+import os
+from datetime import datetime
+
+
+LOG_FILE_NAME = f"{datetime.now().strftime('%m%d%Y__%H%M%S')}.log"
+LOG_FILE_DIR = os.path.join(os.getcwd(), 'logs')
+
+#create directory if not exist
+os.makedirs(LOG_FILE_DIR, exist_ok=True)
+
+LOG_FILE_PATH = os.path.join(LOG_FILE_DIR, LOG_FILE_NAME)
+
+logging.basicConfig(
+    filename=LOG_FILE_PATH,
+    level=logging.INFO,
+    format="[ %(asctime)s ] %(lineno)d %(name)s - %(levelname)s - %(message)s",
+)
